eb9f4d180a7598f4d384f421f5e29fbc266d18a6
Henrique Abner <102832927+HenriqueAbn@users.noreply.github.com>
2023-05-10 20:36:46 -0300

    Aprimoramento na Homepage

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,14 @@ const imageUrl = import.meta.env.VITE_IMG
 
 const MovieCard = ({movie, showLink = true}) => {
   return <div className="movie-card">
-    <img src={imageUrl + movie.poster_path} alt={movie.title} />
+    
+    {showLink && <Link to={`/movie/${movie.id}`}>
+      <img src={imageUrl + movie.poster_path} alt={movie.title} />
     <h2>{movie.title}</h2>
     <p>
         <FaStar /> {movie.vote_avarage}
     </p>
-    {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+      </Link>}
   </div>
 }
 
